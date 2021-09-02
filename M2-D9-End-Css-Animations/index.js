9158f3ed270589b86728d73a7be801f4566e583f
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-09-02 16:31:29 +0200

    button elevate itself

--- M2-D9-End-Css-Animations/index.js
+++ M2-D9-End-Css-Animations/index.js
@@ -1,9 +1,7 @@
-function onClickDeleteRow() {
-    let row = document.querySelectorAll("tr")
-    let deleteBtn = document.querySelectorAll("btn btn-primary")
-    deleteBtn.addEventListener("click" , () => {
-        row.remove()
-    })
-}
+let removeButtons = document.getElementsByClassName("del");
 
-onClickDeleteRow()
+Array.from(removeButtons).forEach((removeButton) => {
+  removeButton.addEventListener("click", () => {
+    removeButton.parentNode.parentNode.remove();
+  });
+});
