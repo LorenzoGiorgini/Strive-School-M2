068d360c285ac325981b07aa4e412bbf34b5f3b7
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-09-03 22:21:30 +0200

    updated the function
    now it's better

--- M2-D9-End-Css-Animations/index.js
+++ M2-D9-End-Css-Animations/index.js
@@ -1,7 +1,22 @@
-let removeButtons = document.getElementsByClassName("del");
+/* let removeButtons = document.getElementsByClassName("del");
 
 Array.from(removeButtons).forEach((removeButton) => {
   removeButton.addEventListener("click", () => {
     removeButton.parentNode.parentNode.remove();
   });
-});
+}); */
+
+
+function onClickRemove() {
+  let btn = document.querySelectorAll(".del")
+  console.log(btn)
+  let row = document.querySelectorAll(".effect-hover")
+  console.log(row)
+  for (let i = 0; i < row.length; i++) {
+    btn[i].addEventListener("click" , () =>{
+      row[i].remove()
+    })
+  }
+}
+
+onClickRemove()
